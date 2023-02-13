0da4daaed3e410b1242bb58f3d5181dbea696dd5
Patrick Dube <dubepatrick@outlook.com>
2023-02-12 22:39:13 -0500

    updated PlayerMovement and LevelChanger code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 5f; // the speed of the player
+
+    private Rigidbody2D rigidBody;
+    private Vector2 movement;
+
+    private void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Get input for movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // Set the movement vector
+        movement = new Vector2(horizontal, vertical);
+
+        // Normalize the movement vector so the player moves at a constant speed
+        movement = movement.normalized * speed * Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        // Move the rigidbody using the movement vector
+        rigidBody.MovePosition(rigidBody.position + movement);
     }
 }
